d6595c23ac340e9656b7d01395f2b9b14431b27b
marcelo9987 <26576174+marcelo9987@users.noreply.github.com>
2024-10-26 00:16:28 +0200

    - He mejorado la gestión de imagenes de Thing.java. Esta aún necesita mejora, ya que es una gestión muy rudimentaria e incluso incorrecta (estoy usando Exception)

--- PuidgemontLaFuga/src/main/java/org/samumarcesoft/lafuga/objetos/Thing.java
+++ PuidgemontLaFuga/src/main/java/org/samumarcesoft/lafuga/objetos/Thing.java
@@ -1,25 +1,41 @@
 package org.samumarcesoft.lafuga.objetos;
 
-import jdk.jshell.spi.ExecutionControl;
 import org.samumarcesoft.lafuga.Drawable;
-import org.samumarcesoft.lafuga.enums.Direccion;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 
 public class Thing implements Drawable {
 
     private String imageAddress;
 
+    private Image imagen;
 
-    public Boolean setImage(String imageAddress)
-    {
-        if(!new File(imageAddress).isFile())
-        {
+    @Override
+    public void draw() {
+    }
+
+    public Boolean setImage(String imageAddress) {
+        if (!new File(imageAddress).isFile()) {
             return false;
         }
+        try {
+            this.imagen = ImageIO.read(new File(imageAddress));
+            //todo: ¿Cámbiame?
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
         this.imageAddress = imageAddress;
 
         return true;
     }
 
+
+    public Icon getImageInIcon() {
+        return new ImageIcon(imagen);
+    }
 }
